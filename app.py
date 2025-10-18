30663b821cc32ca0bed2932597eb83f1e7d6f63c
Mark Meshcheryakov <markzum@yandex.ru>
2025-10-18 10:02:23 +0300

    Сделана страница с трейлером

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timezone
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 
 def create_app() -> Flask:
@@ -22,12 +22,15 @@ def create_app() -> Flask:
 
     @app.route("/trailer")
     def trailer():
-        pass
-        # return render_template("trailer.html")
+        return render_template("trailer.html")
 
     @app.route("/team")
     def team():
         return render_template("team.html")
+
+    @app.route('/big_files/<path:filename>')
+    def big_files(filename):
+        return send_from_directory('big_files', filename)
 
     return app
 
@@ -50,6 +53,6 @@ if __name__ == "__main__":
             print("openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365")
     else:
         # Обычный запуск
-        print("🚀 Запуск сервера...")
-        print("🎯 Полная версия: http://127.0.0.1:3750/team")
-        app.run(host="127.0.0.1", port=3750, debug=True)
+        # print("🚀 Запуск сервера...")
+        # print("🎯 Полная версия: http://127.0.0.1:3750/team")
+        app.run(host="127.0.0.1", port=3750, debug=False)
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,7 @@ def test_routes_exist():
     app = create_app()
     client = app.test_client()
 
-    for path in ("/", "/presentation", "/team"):
+    for path in ("/", "/presentation", "/trailer", "/team"):
         response = client.get(path)
         assert response.status_code == 200
         assert b"<!DOCTYPE html>" in response.data
